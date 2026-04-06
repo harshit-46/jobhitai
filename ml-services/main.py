da28f146b925169bb9e5a8af5084e3fca31748e5
Harshit Gupta <iamharshit999@gmail.com>
2026-04-06 13:38:35 +0530

    update

--- ml-services/main.py
+++ ml-services/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from services import resume_classifier, skill_matcher, job_matcher, career
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def root():

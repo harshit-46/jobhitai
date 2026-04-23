b6b398ccbeeb9c8a45103486587e5cfee3e730d8
Harshit Gupta <iamharshit999@gmail.com>
2026-04-23 12:55:41 +0530

    update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -256,7 +256,7 @@ export default function Dashboard() {
 
     useEffect(() => {
         if (user) {
-            setShowUpload(!user.has_Resume);
+            setShowUpload(!user?.has_Resume);
         }
     }, [user]);
 
